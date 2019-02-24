a4d357589ff815eceaf917bc6a94dad75d7f04c4
Adam Guest <chamook@live.dk>
2019-02-24 18:04:59 +0100

    Configure app to read colours from api

--- Colours/Colours/ViewController.swift
+++ Colours/Colours/ViewController.swift
@@ -8,13 +8,123 @@
 
 import UIKit
 
+struct Colour: Decodable {
+    var id: String
+    var name: String
+    var hex: String
+}
+
 class ViewController: UIViewController {
 
+    var headerView: UIView?
+    var headerLabel: UILabel?
+    var tableView: UITableView?
+    
+    var colours: [Colour] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.installHeader()
+        self.installTableView()
+        
+        self.loadColours()
     }
+    
+    private func loadColours() {
+        let url = URL(string: "http://localhost:5000/my-colours")
+        
+        URLSession.shared.dataTask(with: url!) { [weak self] (data, response, error) in
+            guard error == nil else {
+                print("Error getting colours")
+                return
+            }
+            guard let content = data else {
+                print("Couldn't get any colours")
+                return
+            }
+            
+            do {
+                struct ColourDto: Decodable {
+                    var colours: [Colour]
+                }
+                let colourData = try JSONDecoder().decode(ColourDto.self, from: content)
+                self?.colours = colourData.colours
+                
+                DispatchQueue.main.async {
+                    self?.tableView?.reloadData()
+                }
+            } catch let err {
+                print("Error decoding json", err)
+            }
+        }.resume()
+    }
+    
+    private func installTableView() {
+        guard let header = self.headerView else {
+            return
+        }
+        
+        let table = UITableView()
+        table.backgroundColor = .white
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "colour")
+        self.view.addSubview(table)
+        
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.topAnchor.constraint(equalTo: header.bottomAnchor).isActive = true
+        table.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        table.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        
+        table.dataSource = self
+        self.tableView = table
+    }
+    
+    private func installHeader() {
+        let header = UIView()
+        header.backgroundColor = .white
+        self.view.addSubview(header)
+        
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        header.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        header.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        header.heightAnchor.constraint(equalToConstant: CGFloat(100)).isActive = true
+        
+        let label = UILabel()
+        label.text = "My Colours"
+        label.textAlignment = .center
+        header.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.bottomAnchor.constraint(equalTo: header.bottomAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: header.leadingAnchor).isActive = true
+        label.trailingAnchor.constraint(equalTo: header.trailingAnchor).isActive = true
+        
+        self.headerView = header
+        self.headerLabel = label
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colours.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "colour") {
+            cell.backgroundColor = .white
+            
+            if let label = cell.textLabel {
+                label.text = "\(colours[indexPath.item].name) (\(colours[indexPath.item].hex))"
+            }
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    
 }
 
